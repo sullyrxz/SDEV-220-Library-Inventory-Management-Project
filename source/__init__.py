c2c2901b6c09e6f3f46d90629c9f1da9cfb05ab7
WangBo-YL <bwang18@ivytech.edu>
2024-02-22 18:18:50 -0500

    Try to build the project file structure and configure the project environment. flask and flask_alchemy frameworks are used.

---
+++ source/__init__.py
@@ -0,0 +1,15 @@
+#configure Flask and database
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+#create Flask application
+app = Flask(__name__)
+
+#configure database
+#specify database URI
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+#turn off Flask-SQLAlchemy's track modifications to increase performance
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+#initialize database
+db = SQLAlchemy(app)
